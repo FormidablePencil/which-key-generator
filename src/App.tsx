f1a01edc608f0acd29968f47663e672769d587cc
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-02-04 18:49:45 -0800

    finished-custom-whichkeys

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
   const [value1, setValue1] = useState('')
   const [value2, setValue2] = useState('')
-  
-  return (
-    <>
-<input type='text' value={value1} onChange={(e) => setValue1(e.target.value)} />
-<input type='text' value={value2} onChange={(e) => setValue2(e.target.value)} />
 
-  <div>\  '{value1}' : ['','{value2}'],</div>
-   </>
+  return (
+    <div>
+      <input type='text' placeholder='cmd' value={value1} onChange={(e) => setValue1(e.target.value)} />
+      <input type='text' placeholder='description' value={value2} onChange={(e) => setValue2(e.target.value)} />
+
+      <div>\  '{value1}' : ['','{value2}'],</div>
+    </div>
   );
 }
 
